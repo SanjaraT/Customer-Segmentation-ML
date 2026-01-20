9e6686e72df823c62b0e1f45ab4257a103255691
sanjaraT <sanjaratunola@gmail.com>
2026-01-20 12:59:14 +0600

    EDA

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import seaborn as sns 
+import matplotlib.pyplot as plt
 
 
 df = pd.read_csv("Mall_Customers.csv")
@@ -7,6 +9,23 @@ df = pd.read_csv("Mall_Customers.csv")
 # print(df.shape)
 # print(df.info())
 
-print(df.describe())
-print(df.isnull().sum())
-print(df.duplicated().sum())
+# print(df.describe())
+# print(df.isnull().sum())
+# print(df.duplicated().sum())
+
+#Distribution
+num_cols = ['CustomerID','Age','AnnualIncome','SpendingScore']
+for col in num_cols:
+    sns.histplot(df[col], kde=True)
+    plt.title("Distribution")
+    # plt.show()
+
+#Boxplots
+for col in num_cols:
+    sns.boxplot(x=df[col])
+    plt.title(f'Boxplot of {col}')
+    # plt.show()
+
+#Pairplots
+sns.pairplot(df[num_cols])
+plt.show()
