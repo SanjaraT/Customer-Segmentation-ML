60b5b0777a3e1903c765ec006d8095cc78d8f4f3
sanjaraT <sanjaratunola@gmail.com>
2026-01-20 12:46:38 +0600

    Dataset Summary

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import pandas as pd
 
 
 df = pd.read_csv("Mall_Customers.csv")
-print(df.head())
-print(df.shape)
-print(df.info())
+# print(df.head())
+# print(df.shape)
+# print(df.info())
+
+print(df.describe())
+print(df.isnull().sum())
+print(df.duplicated().sum())
